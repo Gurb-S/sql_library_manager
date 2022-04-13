7eeb1b36e7b6ae307916dfee4cd4392fef533680
Gurb-S <OrganicMintz@gmail.com>
2022-04-13 06:58:23 +0200

    finished all non-error routes

--- routes/index.js
+++ routes/index.js
@@ -28,15 +28,48 @@ router.get('/books/new', asyncHandler(async(req,res) =>{
 }));
 
 router.post('/books/new',asyncHandler(async(req,res) =>{
-  const book = await Book.create(req.body);
-  console.log(req.body);
-  res.redirect('/books')
+  try {
+    console.log('INITIAL BODY', req.body);
+    const book = await Book.create({
+      title: req.body.title,
+      author: req.body.Author,
+      genre: req.body.genre,
+      year: req.body.year
+    });
+    console.log(book.toJSON())
+    res.redirect('/books')
+  } catch (error) {
+    if(error.name === 'SequelizeValidationError'){
+      const errors = error.errors.map(err => err.message);
+      const errorsPath = error.errors.map(err => err.path);
+      console.error('Validation errors: ', errors)
+      console.log(errorsPath)
+      res.render('new-book', { errorsPath })
+    }
+  }
+
 }));
 
 router.get('/books/:id', asyncHandler(async(req,res) =>{
   const book = await Book.findByPk(req.params.id);
-  console.log(book.toJSON())
-  res.render('update-book', { book })
+  res.render('update-book',{ book })
+}));
+
+router.post('/books/:id', asyncHandler(async(req,res) =>{
+  const book = await Book.findByPk(req.params.id);
+  await book.update({
+    title: req.body.title,
+    author: req.body.Author,
+    genre: req.body.genre,
+    year: req.body.year
+  })
+  res.redirect('/books');
+}))
+
+router.post('/books/:id/delete', asyncHandler(async(req,res) =>{
+  const deleteBook = await Book.findByPk(req.params.id);
+  await deleteBook.destroy();
+  res.redirect('/');
 }));
 
 module.exports = router;
